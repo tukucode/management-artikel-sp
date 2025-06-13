5dcafa7c4f0c2d8028b2dc8efe43955b99fef9b1
tomimandalaputra <tommymandalaputra@gmail.com>
2025-06-13 21:34:28 +0700

    refactor form register

--- src/app/auth/register/form.tsx
+++ src/app/auth/register/form.tsx
@@ -1,35 +1,35 @@
 'use client'
 
 import Link from 'next/link'
+import { Input } from '@/components/ui/input'
+import { FormButtonSubmit } from '@/components/form/button-submit'
+import { Select, SelectTrigger, SelectValue, SelectContent, SelectItem } from '@/components/ui/select'
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form'
+
 import { useState } from 'react'
 import { redirect } from 'next/navigation'
 import { useForm } from 'react-hook-form'
-import { zodResolver } from '@hookform/resolvers/zod'
 import { $axios } from '@/lib/axios'
-import { FormButtonSubmit } from '@/components/form/button-submit'
-import { FormField } from '@/components/form/field'
+import { zodResolver } from '@hookform/resolvers/zod'
 import { Role  } from '@/types/role_type'
-import { FormDataAuth, authSchema } from '@/lib/schemas/authSchema'
+import { registerSchema, RegisterFormData } from '@/lib/schemas/authSchema'
 import { ResponseRegister } from '@/types/responses/register_response_type'
 
 export const FormRegister = () => {
   const [isLoding, setLoading] = useState(false)
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm<FormDataAuth>({
-    resolver: zodResolver(authSchema),
+  const form = useForm<RegisterFormData>({
+    resolver: zodResolver(registerSchema),
+    defaultValues: {
+      username: '',
+      role: undefined,
+      password: '',
+    },
   })
 
-  const onSubmit = async (formData: FormDataAuth) => {
+  const onSubmit = async (formData: RegisterFormData) => {
     try {
       setLoading(true)
-      await $axios.post<ResponseRegister>('/auth/register', {
-        ...formData,
-        role: Role.User,
-      })
-
+      await $axios.post<ResponseRegister>('/auth/register', formData)
       redirect('/auth/login')
     } finally {
       setLoading(false)
@@ -37,39 +37,67 @@ export const FormRegister = () => {
   }
 
   return (
-    <form id='form__register' onSubmit={handleSubmit(onSubmit)}>
-      <div className="flex flex-col gap-6">
-        <div className="grid gap-2">
-          <FormField<FormDataAuth>
-            label="username"
-            type="text"
-            name="username"
-            register={register}
-            error={errors.username}
-          />
-        </div>
+    <Form {...form}>
+      <form id='form__register' onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+        <FormField
+          control={form.control}
+          name="username"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Username</FormLabel>
+              <FormControl>
+                <Input {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
 
-        <div className="grid gap-2">
-          <FormField<FormDataAuth>
-            label="password"
-            type="password"
-            name="password"
-            register={register}
-            error={errors.password}
-          />
-        </div>
+        <FormField
+          control={form.control}
+          name="role"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Role</FormLabel>
+              <Select onValueChange={field.onChange} value={field.value}>
+                <FormControl className='w-full'>
+                  <SelectTrigger>
+                    <SelectValue placeholder="Select a role" />
+                  </SelectTrigger>
+                </FormControl>
+                <SelectContent>
+                  <SelectItem value={Role.User}>User</SelectItem>
+                  <SelectItem value={Role.Admin}>Admin</SelectItem>
+                </SelectContent>
+              </Select>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
 
-        <div className="grid gap-4 text-center">
-          <FormButtonSubmit isLoading={isLoding} label='Register' />
-          <p>
-            Alredy have an account? {' '}
-            <Link href="/auth/login" className='text-blue-500'>
-              Login
-            </Link>
-            .
-          </p>
-        </div>
-      </div>
-    </form>
+        <FormField
+          control={form.control}
+          name="password"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Password</FormLabel>
+              <FormControl>
+                <Input type="password" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormButtonSubmit isLoading={isLoding} label='Register' />
+        <p className='text-center'>
+          Alredy have an account? {' '}
+          <Link href="/auth/login" className='font-bold'>
+            Login
+          </Link>
+          .
+        </p>
+      </form>
+    </Form>
   )
 }
