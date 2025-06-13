4ed961815f9199e93707fba5dfc3112c8629d2c4
tomimandalaputra <tommymandalaputra@gmail.com>
2025-06-13 20:13:04 +0700

    chore: slicing ui page not found

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import Link from 'next/link'
+import { Button } from '@/components/ui/button'
 
-export default function NotFound() {
+export default function ForbiddenPage() {
   return (
-    <div>
-      <h1>404 | Not Found</h1>
+    <div className="min-h-screen flex flex-col items-center justify-center text-center">
+      <h1 className="text-4xl font-bold">404 - Page Not Found</h1>
+      
+      <p className="my-4 text-lg">
+        We apologize, but the page you are not looking for does not exist.
+      </p>
+
+      <Button asChild>
+        <Link href="/">
+          Back to Home
+        </Link>
+      </Button>
     </div>
   )
 }
