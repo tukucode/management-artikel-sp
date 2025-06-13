753151bf015ad11a74e7e1769fe8440811aa275a
tomimandalaputra <tommymandalaputra@gmail.com>
2025-06-13 20:34:17 +0700

    chore: added logic for handle access page on middleware

--- src/middleware.tsx
+++ src/middleware.tsx
@@ -1,15 +1,45 @@
-import { NextResponse } from 'next/server'
-import type { NextRequest } from 'next/server'
+import { NextRequest, NextResponse } from 'next/server'
 
 export function middleware(request: NextRequest) {
   const { pathname } = request.nextUrl
-  console.log('PUBLIC:', pathname)
+  const token = request.cookies.get('token')?.value
+  const role = request.cookies.get('role')?.value
+
+  const isAuthRoute = pathname.startsWith('/auth')
+  const isLandingRoute = pathname.startsWith('/landing')
+  const isDashboardRoute = pathname.startsWith('/dashboard')
+
+  if (!token && (isDashboardRoute || isLandingRoute)) {
+    return NextResponse.redirect(new URL('/auth/login', request.url))
+  }
+
+  if (token && role) {
+    if (['user'].includes(role.toLocaleLowerCase()) && isDashboardRoute) {
+      return NextResponse.redirect(new URL('/403', request.url))
+    }
+
+    if (['admin'].includes(role.toLocaleLowerCase()) && isLandingRoute) {
+      return NextResponse.redirect(new URL('/403', request.url))
+    }
+
+    if (!['admin', 'user'].includes(role.toLocaleLowerCase())) {
+      return NextResponse.redirect(new URL('/403', request.url))
+    }
+  }
+
+  if (isAuthRoute) {
+    if (role === 'User') {
+      return NextResponse.redirect(new URL('/landing/article', request.url))
+    }
+
+    if (role === 'Admin' ) {
+      return NextResponse.redirect(new URL('/dashboard/article', request.url))
+    }
+  }
+
   return NextResponse.next()
 }
 
 export const config = {
-  matcher: [
-    '/dashboard/:path*',
-    '/article',
-  ],
+  matcher: ['/auth/:path*', '/dashboard/:path*', '/landing/:path*'],
 }
