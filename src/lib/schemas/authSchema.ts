7b528ec406ea99060c8843db917ee0c1c2d31ea3
tomimandalaputra <tommymandalaputra@gmail.com>
2025-06-13 21:34:57 +0700

    refactor authSchema

--- src/lib/schemas/authSchema.ts
+++ src/lib/schemas/authSchema.ts
@@ -1,19 +1,24 @@
 import { z } from 'zod'
 import { Role } from '@/types/role_type'
 
-export type FormDataAuth = {
-  username: string
-  password: string
-  role?: Role
-}
+const usernameSchema = z
+  .string()
+  .min(3, { message: 'Username must be at least 3 characters' })
+  .max(20, { message: 'Username must be at most 20 characters' })
 
-export const authSchema = z.object({
-  username: z
-    .string()
-    .min(3, { message: 'Username must be at least 3 characters' })
-    .max(20, { message: 'Username must be at most 20 characters' }),
-  password: z
-    .string()
-    .min(8, { message: 'Password must be at least 8 characters' })
-    .max(12, { message: 'Password must be at most 12 characters' }),
+const passwordSchema = z
+  .string()
+  .min(8, { message: 'Password must be at least 8 characters' })
+  .max(12, { message: 'Password must be at most 12 characters' })
+
+export const loginSchema = z.object({
+  username: usernameSchema,
+  password: passwordSchema,
 })
+
+export const registerSchema = loginSchema.extend({
+  role: z.nativeEnum(Role, { required_error: 'Role is required' }),
+})
+
+export type LoginFormData = z.infer<typeof loginSchema>;
+export type RegisterFormData = z.infer<typeof registerSchema>;
