94a3d241a7c7f4a02558595a4e5e2dc64d2c6eba
tomimandalaputra <tommymandalaputra@gmail.com>
2025-06-13 21:34:20 +0700

    refactor from login

--- src/app/auth/login/form.tsx
+++ src/app/auth/login/form.tsx
@@ -1,70 +1,77 @@
 'use client'
 
 import Link from 'next/link'
-import { useState } from 'react'
-import { redirect } from 'next/navigation'
-import { useForm } from 'react-hook-form'
-import { zodResolver } from '@hookform/resolvers/zod'
-import { $axios } from '@/lib/axios'
+import { Input } from '@/components/ui/input'
 import { FormButtonSubmit } from '@/components/form/button-submit'
-import { FormField } from '@/components/form/field'
-import { FormDataAuth, authSchema } from '@/lib/schemas/authSchema'
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form'
+
+import { useState } from 'react'
+import { useForm } from 'react-hook-form'
+import { $axios } from '@/lib/axios'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { loginSchema, LoginFormData } from '@/lib/schemas/authSchema'
 import { ResponseLogin } from '@/types/responses/login_response_type'
 
 export const FormLogin = () => {
   const [isLoding, setLoading] = useState(false)
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm<FormDataAuth>({
-    resolver: zodResolver(authSchema),
+  const form = useForm<LoginFormData>({
+    resolver: zodResolver(loginSchema),
+    defaultValues: {
+      username: '',
+      password: '',
+    },
   })
 
-  const onSubmit = async (formData: FormDataAuth) => {
+  const onSubmit = async (formData: LoginFormData) => {
     try {
       setLoading(true)
-      await $axios.post<ResponseLogin>('/auth/login', formData)
-      redirect('/landing/article')
+      const response = await $axios.post<ResponseLogin>('/auth/login', formData)
+      const location = response.data.data.role.toLocaleLowerCase() == 'admin' ? '/dashboard/article' : '/landing/article'
+      window.location.replace(location)
     } finally {
       setLoading(false)
     }
   }
 
   return (
-    <form id='form__register' onSubmit={handleSubmit(onSubmit)}>
-      <div className="flex flex-col gap-6">
-        <div className="grid gap-2">
-          <FormField<FormDataAuth>
-            label="username"
-            type="text"
-            name="username"
-            register={register}
-            error={errors.username}
-          />
-        </div>
+    <Form {...form}>
+      <form id='form__register' onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+        <FormField
+          control={form.control}
+          name="username"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Username</FormLabel>
+              <FormControl>
+                <Input {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
 
-        <div className="grid gap-2">
-          <FormField<FormDataAuth>
-            label="password"
-            type="password"
-            name="password"
-            register={register}
-            error={errors.password}
-          />
-        </div>
+        <FormField
+          control={form.control}
+          name="password"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Password</FormLabel>
+              <FormControl>
+                <Input type="password" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
 
-        <div className="grid gap-4 text-center">
-          <FormButtonSubmit isLoading={isLoding} label='Login' />
-          <p>
-            Don&apos;t have an account? {' '}
-            <Link href="/auth/register" className='text-blue-500'>
-              Register
-            </Link>
-            .
-          </p>
-        </div>
-      </div>
-    </form>
+        <FormButtonSubmit isLoading={isLoding} label='Login' />
+        <p className='text-center'>
+          Don&apos;t have an account yet? {' '}
+          <Link href="/auth/register" className='font-bold'>
+            Register
+          </Link>
+        </p>
+      </form>
+    </Form>
   )
 }
