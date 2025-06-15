62ae2a3f618c6ea9f72729b77ee8837808020f26
tomimandalaputra <tommymandalaputra@gmail.com>
2025-06-16 00:03:07 +0700

    added formateDateTime

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,17 @@ import { twMerge } from 'tailwind-merge'
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function formatDateTime(isoString: string): string {
+  const date = new Date(isoString)
+
+  const day = String(date.getDate()).padStart(2, '0')
+  const month = String(date.getMonth() + 1).padStart(2, '0')
+  const year = date.getFullYear()
+
+  const hours = String(date.getHours()).padStart(2, '0')
+  const minutes = String(date.getMinutes()).padStart(2, '0')
+
+  return `${day}/${month}/${year} ${hours}:${minutes}`
+}
+
